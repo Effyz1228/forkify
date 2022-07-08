8f2206b5086f8953d9923724b9140a6a0dd0842b
Effy Zhang <effyz1228@gmail.com>
2022-07-08 14:07:59 -0700

    initial commit

---
+++ src/js/model.js
@@ -0,0 +1,33 @@
+import { async } from 'regenerator-runtime';
+
+export const state = {
+  recipe: {},
+};
+
+export const loadRecipe = async function (id) {
+  try {
+    const res = await fetch(
+      // 'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
+    );
+
+    const data = await res.json();
+    if (!res.ok) throw new Error(`${data.message},(${res.status})`);
+
+    //destructuring
+    let { recipe } = data.data;
+    state.recipe = {
+      cookingTime: recipe.cooking_time,
+      id: recipe.id,
+      image: recipe.image_url,
+      ingredients: recipe.ingredients,
+      publisher: recipe.publisher,
+      servings: recipe.servings,
+      title: recipe.title,
+      sourceUrl: recipe.source_url,
+    };
+    console.log(state.recipe);
+  } catch (err) {
+    alert(err);
+  }
+};
