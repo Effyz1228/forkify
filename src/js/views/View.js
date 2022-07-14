3930f871ad423b7dd7edc8fdb6f8962b9d87fb40
Effy Zhang <effyz1228@gmail.com>
2022-07-13 22:58:19 -0700

    implement only update different data in recipeView

--- src/js/views/View.js
+++ src/js/views/View.js
@@ -18,6 +18,37 @@ export default class View {
     this._parentElement.innerHTML = '';
   }
 
+  update(data) {
+    if (!data || (Array.isArray(data) && data.length === 0))
+      return this.renderError();
+
+    this._data = data;
+    const newMarkups = this._generateMarkup();
+    //newMarkups is just a string, very hard to compare
+    //a trick to solve this problem
+    const newDOM = document.createRange().createContextualFragment(newMarkups);
+    const newElements = Array.from(newDOM.querySelectorAll('*'));
+    const currElements = Array.from(this._parentElement.querySelectorAll('*'));
+    newElements.map((newEl, i) => {
+      const curEl = currElements[i];
+
+      //update changed Text
+      if (
+        !newEl.isEqualNode(curEl) &&
+        newEl.firstChild?.nodeValue.trim() !== ''
+      ) {
+        //console.log(newEl.firstChild?.nodeValue.trim());
+        curEl.innerHTML = newEl.innerHTML;
+      }
+      //update changed attribute (dataset on btn)
+      if (!newEl.isEqualNode(curEl)) {
+        Array.from(newEl.attributes).map(attr => {
+          curEl.setAttribute(attr.name, attr.value);
+        });
+      }
+    });
+  }
+
   renderSpinner() {
     const markup = `
             <div class="spinner">
