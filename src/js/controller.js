d8008464c63317fc1b2fa60fb2baaa6e812765a6
Effy Zhang <effyz1228@gmail.com>
2022-07-20 10:45:38 -0700

    fix bugs

--- src/js/controller.js
+++ src/js/controller.js
@@ -3,8 +3,8 @@ import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
 import paginationView from './views/paginationView.js';
-import bookmarkView from './views/BookmarkView.js';
-import addRecipeView from './views/AddRecipeView.js';
+import bookmarkView from './views/bookmarkView.js';
+import addRecipeView from './views/addRecipeView.js';
 import { MODAL_CLOSE_SEC } from './config.js';
 
 //polyfill
